f43011bd7a177c8297d37bc83285046cc1d4bbfa
cchavezmx99 <carlos.chavez@99minutos.com>
2023-07-16 03:42:34 -0600

    probando astro

---
+++ src/data/books.js
@@ -0,0 +1,38 @@
+const books = [
+  { 
+    id: 'clean-code',
+    title: 'Clean Code: Manual de desarrollo ágil de software',
+    link: 'https://www.amazon.com/-/es/Robert-C-Martin/dp/0132350882',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX258_BO1,204,203,200_.jpg',
+    author: 'Robert C. Martin',
+    opinion: 'Un libro que todo programador debería leer. Enseña a escribir código limpio, entendible y mantenible. Es un libro que se puede leer de principio a fin o como referencia para consultar cuando se necesite.'
+
+  },
+  {
+    id: 'the-clean-coder',
+    title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+    link: 'https://www.amazon.com/-/es/Robert-C-Martin/dp/0137081073',
+    image: 'https://m.media-amazon.com/images/I/51uO-K+V5dL._AC_UF1000,1000_QL80_.jpg',
+    author: 'Robert C. Martin',
+    opinion: 'Un libro que todo programador debería leer. Enseña a escribir código limpio, entendible y mantenible. Es un libro que se puede leer de principio a fin o como referencia para consultar cuando se necesite.'
+  },
+  {
+    id: 'javascript-the-good-parts',
+    title: 'JavaScript: The Good Parts',
+    link: 'https://www.amazon.com/-/es/Douglas-Crockford/dp/0596517742',
+    image: 'https://m.media-amazon.com/images/I/81kqrwS1nNL._AC_UF1000,1000_QL80_.jpg',
+    author: 'Douglas Crockford',
+    opinion: 'Un libro que todo programador debería leer. Enseña a escribir código limpio, entendible y mantenible. Es un libro que se puede leer de principio a fin o como referencia para consultar cuando se necesite.'
+  },
+  {
+    id: 'eloquent-javascript',
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    link: 'https://www.amazon.com/-/es/Marijn-Haverbeke/dp/1593275846',
+    image: 'https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UF1000,1000_QL80_.jpg',
+    author: 'Marijn Haverbeke',
+    opinion: 'Un libro que todo programador debería leer. Enseña a escribir código limpio, entendible y mantenible. Es un libro que se puede leer de principio a fin o como referencia para consultar cuando se necesite.'
+  },
+  
+]
+
+export default books;
